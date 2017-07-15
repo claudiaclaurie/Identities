db4d7e80935a92ba6c16c9a8b59e700c46d73f34
Claudia Laurie <claudiaclaurie@college.harvard.edu>
2017-07-15 14:36:21 -0700

    getting the layout right

--- Identities/ViewController1.swift
+++ Identities/ViewController1.swift
@@ -2,8 +2,9 @@ import UIKit
 
 class ViewController1: UIViewController{
     var scrollView = UIScrollView()
-    var imageView = UIImageView(image: UIImage(named: "asia.jpg")!)
-    let labelInst = UILabel()
+    var imageView1 = UIImageView(image: UIImage(named: "asia.jpg")!)
+    let TitleLabel = UILabel()
+    let CaptionLabel1 = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -12,33 +13,41 @@ class ViewController1: UIViewController{
         
         //image
         
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
+        imageView1.contentMode = .scaleAspectFill
+        imageView1.clipsToBounds = true
         
         self.view.backgroundColor = UIColor.yellow
         
-        // label
-        labelInst.text = "Page 1"
+        // label 1
+        TitleLabel.text = "Featured"
+        TitleLabel.textAlignment = .center
+        
+        // label 2 (caption)
+        CaptionLabel1.text = "Gallery: Highlights from the Runway"
 
         // Scroll View
         scrollView.alwaysBounceVertical = true
 
         //self.view.addSubview(scrollView)
-        self.scrollView.addSubview(imageView)
-        self.scrollView.addSubview(labelInst)
+        self.scrollView.addSubview(imageView1)
+        self.scrollView.addSubview(TitleLabel)
+        self.scrollView.addSubview(CaptionLabel1)
         self.view.addSubview(scrollView)
         
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-
+        //let w = view.bounds.width
         //Position subViews on screen.
         self.scrollView.frame = view.bounds
-        self.labelInst.frame = CGRect(x:0,y:0,width:self.view.bounds.width, height:50)
-        self.imageView.frame = CGRect(x:0, y:20, width: 180, height: 340)
+        self.TitleLabel.frame = CGRect(x:0,y:0,width:self.view.bounds.width, height:50)
+        self.imageView1.frame = CGRect(x:0, y:40, width: self.view.bounds.width, height: 500)
+        self.CaptionLabel1.frame = CGRect(x:4, y: 550, width: self.view.bounds.width, height: 30)
+        //Sizing the content that shows up on the window of the scrollview- should be size of all the content you want to scroll
+        //Moving paper behind window
         self.scrollView.contentSize = CGSize(width: view.bounds.width,
-                                             height: imageView.frame.origin.y + imageView.frame.size.height)
+                                             height: CaptionLabel1.frame.origin.y + CaptionLabel1.frame.size.height)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
